58cd36fb42fc497c69e662b8d714e7fb0120be70
Danila <staroverovdanila@gmail.com>
2021-11-06 13:06:11 +0300

    Release

--- ZipFake/ZipFake/MainWindow.xaml.cs
+++ ZipFake/ZipFake/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -33,7 +34,53 @@ namespace ZipFake
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
             openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string zipName = Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString())).Remove(15);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                CreateZipFile($"{openFileDialog.InitialDirectory}\\{zipName}.zip", openFileDialog.FileNames);
+                TxbStatus.Text = "Ждите";
+            }
         }
-        
+
+        public async void CreateZipFile(string fileName, IEnumerable<string> files)
+        {
+            try
+            {
+                TxbStatus.Text = "В процессе";
+                await Task.Run(() =>
+                {
+                    var zip = ZipFile.Open(fileName, ZipArchiveMode.Create);
+                    foreach (var file in files)
+                    {
+                        zip.CreateEntryFromFile(file, System.IO.Path.GetFileName(file), CompressionLevel.Optimal);
+                    }
+                    zip.Dispose();
+                    Process.Start("explorer.exe", "/select," + fileName);
+                });
+            }
+            catch (Exception)
+            {
+                GDtop.Background = Brushes.Red;
+                
+                MessageBox.Show("Произошла ошибка, я умираю","О НЕТ",MessageBoxButton.OK,MessageBoxImage.Error);
+
+                await Task.Delay(3000);
+                Process.Start(Application.ResourceAssembly.Location);
+                Application.Current.Shutdown();
+            }
+        }
+        private void DisArchiving_Click(object sender, RoutedEventArgs e)
+        {
+            TxbStatus.Text = "В процессе";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Архивчики(*.zip) | *.zip";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if(openFileDialog.ShowDialog() == true)
+            {
+                string path = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+                ZipFile.ExtractToDirectory(openFileDialog.FileName, path);
+                Process.Start("explorer.exe", "/open," + path);
+            }
+        }
     }
 }
